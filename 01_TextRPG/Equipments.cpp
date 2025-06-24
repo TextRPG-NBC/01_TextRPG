4e154eb9277014302df194d1bb691508ab789fe8
yh1457 <gyh1457@gmail.com>
2025-06-24 17:24:25 +0900

    fix : equip item message
    equip item message

--- 01_TextRPG/Equipments.cpp
+++ 01_TextRPG/Equipments.cpp
@@ -1,6 +1,6 @@
 ﻿#include "Equipments.h"
 #include "Player.h"
-
+#include<iostream>
 
 bool TreeBranch::use(Player& player)
 {
@@ -15,12 +15,16 @@ std::unique_ptr<Item> TreeBranch::clone()
 
 void TreeBranch::equipped(Player& player)
 {
+	std::cout << getName() << "을(를) 장착했습니다." << std::endl;
+	std::cout << "공격력이 10, 치명타율이 5 증가했습니다." << std::endl;
 	player.increaseAttack(10);
 	player.increaseCriticalProbability(5);
 }
 
 void TreeBranch::unEquipped(Player& player)
 {
+	std::cout << getName() << "을(를) 탈착했습니다." << std::endl;
+	std::cout << "공격력이 10, 치명타율이 5 감소했습니다." << std::endl;
 	player.decreaseAttack(10);
 	player.decreaseCriticalProbability(5);
 }
@@ -38,12 +42,17 @@ std::unique_ptr<Item> LeatherArmor::clone()
 
 void LeatherArmor::equipped(Player& player)
 {
+	std::cout << getName() << "을(를) 장착했습니다." << std::endl;
+	std::cout << "최대 체력이 15 증가했습니다." << std::endl;
 	player.increaseMaxHP(15);
 	player.increaseCurrentHP(15);
 }
 
 void LeatherArmor::unEquipped(Player& player)
 {
+	std::cout << getName() << "을(를) 탈착했습니다." << std::endl;
+	std::cout << "최대 체력이 15 감소했습니다." << std::endl;
+
 	player.decreaseMaxHP(15);
 }
 
